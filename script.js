946e40c71c4fa3ac0a1ec3fc7f318ddd01782f4a
BCEESAY10 <bamfaceesay30@gmail.com>
2025-07-13 03:06:30 +0000

    tablet responsiveness

--- script.js
+++ script.js
@@ -3,9 +3,12 @@
 const mainImage = document.getElementById('main-image');
 
 function updateImage() {
-  if (window.innerWidth >= 768) {
+  if (window.innerWidth >= 1200) {
     mainImage.src = 'assets/images/illustration-sign-up-desktop.svg';
-  } else {
+  } else if(window.innerWidth >= 768 && window.innerWidth < 1200){
+    mainImage.src = 'assets/images/illustration-sign-up-tablet.svg';
+  }
+   else {
     mainImage.src = 'assets/images/illustration-sign-up-mobile.svg';
   }
 }
